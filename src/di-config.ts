1a0e97607fbff2d008168daed5e22ef5d2b29965
immortalmind-mo-salah <mohamed.salah@yassir.com>
2024-09-21 13:18:46 +0200

    chore: remove unused module

--- src/di-config.ts
+++ src/di-config.ts
@@ -1,9 +1,6 @@
 import 'reflect-metadata';
 import { Container } from 'inversify';
-import { CustomerService } from './modules/customer/customer.service';
-import { CustomerController } from './modules/customer/customer.controller';
 import { TYPES } from './types';
-import { CustomerRepository } from './modules/customer/customer.repository';
 import { PrismaClientWrapper } from './config/dbClient';
 import { SubscriptionService } from './modules/subscriptions/subscriptions.service';
 import { SubscriptionRepository } from './modules/subscriptions/subscriptions.repository';
@@ -19,11 +16,6 @@ container.bind<PrismaClientWrapper>(PrismaClientWrapper).toSelf().inSingletonSco
 container.bind<SubscriptionService>(TYPES.SubscriptionService).to(SubscriptionService);
 container.bind(TYPES.SubscriptionRepository).to(SubscriptionRepository);
 container.bind(TYPES.SubscriptionController).to(SubscriptionController);
-
-
-container.bind<CustomerService>(TYPES.CustomerService).to(CustomerService);
-container.bind(TYPES.CustomerRepository).to(CustomerRepository);
-container.bind(TYPES.CustomerController).to(CustomerController);
 
 
 
--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,6 @@
-import { CustomerRepository } from "./modules/customer/customer.repository";
 
 export const TYPES = {
-	CustomerService: Symbol.for('CustomerService'),
-	CustomerRepository: Symbol.for('CustomerRepository'),
-	CustomerController: Symbol.for('CustomerController'),
-
 	SubscriptionService: Symbol.for('SubscriptionService'),
 	SubscriptionRepository: Symbol.for('SubscriptionRepository'),
 	SubscriptionController: Symbol.for('SubscriptionController'),
-	// CustomerModel: Symbol.for('CustomerModel'),
-  };
+};
